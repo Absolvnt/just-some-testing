43d23e28073a20f9cfbe9dd574e2289872ddd8d5
Elvis Jakobsson <Elvis.Jakobsson@NiraDynamics.se>
2020-03-20 13:24:15 +0100

    cleanup
    Change-Id: I4aa36871ea494e654952b15c15207c5f7cfbf113

--- src/Letter.hs
+++ src/Letter.hs
@@ -9,8 +9,5 @@ newtype Letter = Letter Int deriving (Eq, Ord, Num)
 instance Show Letter where
     show (Letter x) = show . chr $ (x `mod` 26) + ord 'a'
 
-instance Arbitrary Letter where
-
-
 shift :: Int -> Letter -> Letter
 shift = (+) . Letter
